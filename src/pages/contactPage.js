90c3447532f1cfa137f5dcb481ceea9b9a7d183e
akash fablead <akash.fablead@gmail.com>
2026-04-01 13:19:56 +0530

    removed log

--- src/pages/contactPage.js
+++ src/pages/contactPage.js
@@ -392,7 +392,7 @@ function ContactPage() {
                   <Col lg={6}>
                     <ReCAPTCHA
                       ref={recaptchaRef}
-                      sitekey="6LeARuMUAAAAAE1lFiqVl4FXq8bWKV-xrgRB5y-D"
+                      sitekey="6Lei7aAsAAAAALAHWGTaz0WDj9M2xKmi-fflm9bx"
                       onChange={handleVerification}
                     />
                   </Col>
